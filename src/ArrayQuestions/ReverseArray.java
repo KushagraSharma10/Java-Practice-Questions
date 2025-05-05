46a3936f9ca8b65b63c84ba7cecae6665d97767f
KushagraSharma10 <113988514+KushagraSharma10@users.noreply.github.com>
2025-05-05 17:29:05 +0530

    Reversing an Array, Solution is added

---
+++ src/ArrayQuestions/ReverseArray.java
@@ -0,0 +1,32 @@
+package ArrayQuestions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of Array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+
+        System.out.println("Array before reversing: "+ Arrays.toString(arr));
+        int start = 0, end = size-1;
+        while(start <end){
+            int temp = arr[start];
+            arr[start]=  arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+
+
+        System.out.println("Array after reversing:  "+Arrays.toString(arr));
+    }
+}
