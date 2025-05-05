22d8c692046b9b9147bec61314ba6a063d1d9333
KushagraSharma10 <113988514+KushagraSharma10@users.noreply.github.com>
2025-05-05 16:38:12 +0530

    GreatestElement solution is added

--- src/ArrayQuestions/GreatestElement.java
+++ src/ArrayQuestions/GreatestElement.java
@@ -1,7 +1,23 @@
 package ArrayQuestions;
 
+import java.util.Scanner;
+
 public class GreatestElement {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of Array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
 
+        int greatest = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+
+            if(arr[i] > arr[greatest]){
+                greatest = i;
+            }
+        }
+
+        System.out.println("Greatest Element is: " + arr[greatest] + " at index of " + greatest);
     }
 }
