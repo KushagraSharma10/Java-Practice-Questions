e7585414d6341866da73538823858940beb13f9d
KushagraSharma10 <113988514+KushagraSharma10@users.noreply.github.com>
2025-05-08 17:13:56 +0530

    Bubble sort is added.

---
+++ src/ArrayQuestions/BubbleSort.java
@@ -0,0 +1,28 @@
+package ArrayQuestions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of Array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i< n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < arr.length-1; i++){
+            for(int j = 0; j< arr.length-i-1; j++){
+                if(arr[j]> arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.print("Array after bubble sort: "+ Arrays.toString(arr));
+    }
+}
