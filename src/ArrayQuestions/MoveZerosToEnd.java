1d1b4670cede0f11df357bc6e91cee54fcfb7692
KushagraSharma10 <113988514+KushagraSharma10@users.noreply.github.com>
2025-05-08 16:41:01 +0530

    second method of solving the question move zeros to end is added.

--- src/ArrayQuestions/MoveZerosToEnd.java
+++ src/ArrayQuestions/MoveZerosToEnd.java
@@ -14,19 +14,41 @@ public class MoveZerosToEnd {
             arr[i] = sc.nextInt();
         }
 
-        int[] res = new int[n];
+//        int[] res = new int[n];
+//
+//        int i = 0, j = n-1;
+//
+//        for (int k = 0; k < n; k++) {
+//            if(arr[k] > 0){
+//                res[i++] = arr[k];
+//            }
+//            else{
+//                res[j--] = arr[k];
+//            }
+//        }
+//
+//        System.out.println(Arrays.toString(res));
 
-        int i = 0, j = n-1;
 
-        for (int k = 0; k < n; k++) {
-            if(arr[k] > 0){
-                res[i++] = arr[k];
-            }
-            else{
-                res[j--] = arr[k];
+        // Method 2 without using extra space
+
+        int j = 0;
+        for (int i = 0; i <n ; i++) {
+            if(arr[i] == 0){
+                j = i;
+                break;
             }
         }
 
-        System.out.println(Arrays.toString(res));
+        for (int i = j+1; i <n ; i++) {
+            if(arr[i] != 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 }
