957309f5e8ba6a6368efc9c1669625840c290708
KushagraSharma10 <113988514+KushagraSharma10@users.noreply.github.com>
2025-05-09 17:56:21 +0530

    Solution of question UpdateArray is added.

--- src/ArrayQuestions/UpdateArray.java
+++ src/ArrayQuestions/UpdateArray.java
@@ -6,43 +6,20 @@ public class UpdateArray {
 
         int[] arr = {2, 3, 4, 5, 6};
         int n = arr.length;
-//        int[] ans = new int[n];
-//
-//        for (int i = 0; i < n; i++) {
-//
-//            if (i == 0) {
-//                ans[i] = arr[i] * arr[i + 1];
-//            }
-//            else if (i == n-1) {
-//                ans[i] = arr[i] * arr[i - 1];
-//            }
-//            else{
-//                ans[i] = arr[i - 1] * arr[i + 1];
-//            }
-
-//        System.out.print(ans[i] + " ");
-
-        updateArray(arr, n);
-
-    }
-    public static void updateArray(int[] arr, int n) {
         int[] ans = new int[n];
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
 
-            if(i == 0){
-                ans[i] = arr[i] * arr[i+1];
-            }
-            else if(i == n-1){
-                ans[i] = arr[i] * arr[i-1];
-            }
-            else{
+            if (i == 0) {
+                ans[i] = arr[i] * arr[i + 1];
+            } else if (i == n - 1) {
+                ans[i] = arr[i] * arr[i - 1];
+            } else {
                 ans[i] = arr[i - 1] * arr[i + 1];
             }
 
             System.out.print(ans[i] + " ");
-
         }
-    }
 
+    }
 }
